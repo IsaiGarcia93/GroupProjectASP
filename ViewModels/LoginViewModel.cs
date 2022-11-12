5f25bf3ea36a6fff123f12082485fc1a8a183287
Isai Gonzalez Garcia <ig705295@southeast.edu>
2022-11-12 10:12:21 -0600

    Added the ViewModels folder

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProjectASP.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Please insert a valid email.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Add a valid password.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
