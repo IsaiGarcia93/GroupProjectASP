b743ce398f30ce8fd66619716115c2a6d46eb05a
Isai Gonzalez Garcia <ig705295@southeast.edu>
2022-11-17 15:42:22 -0600

    Customer page, and controller added

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using GroupProjectASP.Data;
+using GroupProjectASP.Models;
+using GroupProjectASP.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProjectASP.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public CustomerController(AppDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Items.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddtoCart(Item item)
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GroupProjectASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProjectASP.ViewModels
+{
+    public class CartViewModel
+    {
+        public IEnumerable<Item> Items { get; set; }
+        public OrderDetails OrderDetails { get; set; }
+    }
+}
