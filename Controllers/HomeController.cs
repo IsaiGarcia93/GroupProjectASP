2485c9f1e74db925b2a951301f12236a1cbec5bb
Isai Gonzalez Garcia <ig705295@southeast.edu>
2022-12-05 20:06:59 -0600

    Carousel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using GroupProjectASP.Models;
+﻿using GroupProjectASP.Data;
+using GroupProjectASP.Models;
+using GroupProjectASP.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,15 +15,24 @@ namespace GroupProjectASP.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDBContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            CarouselViewModel carouselVM = new CarouselViewModel();
+
+            foreach (var item in _context.Items.ToList())
+            {
+                carouselVM.Items = await _context.Items.ToListAsync();
+            }
+
+            return View(carouselVM);
         }
 
         public IActionResult Privacy()
---
+++ ViewModels/CarouselViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GroupProjectASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProjectASP.ViewModels
+{
+    public class CarouselViewModel
+    {
+        public List<Item> Items { get; set; }
+    }
+}
